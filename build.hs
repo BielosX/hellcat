8d2443a28c13086e5f8f60b3484b9c540779bfab
Mateusz Biel <matbiel@gmail.com>
2019-02-10 12:45:41 +0100

    Add shaders check

--- build.hs
+++ build.hs
@@ -5,15 +5,22 @@ import System.Process
 import System.Exit
 import System.FilePath.Posix
 
-callCmd :: String -> [String] -> IO ()
-callCmd cmd args = do
+_callCmd :: String -> [String] -> ((String,String) -> String) -> IO ()
+_callCmd cmd args f = do
     (exitCode, out, err) <- readProcessWithExitCode cmd args []
     case exitCode of
         ExitSuccess -> putStrLn out
-        (ExitFailure _) -> error err
+        (ExitFailure _) -> error $ f (out, err)
+
+callCmd cmd args = _callCmd cmd args snd
 
 stackBuild = callCmd "stack" ["build"]
 stackInstall t = callCmd "stack" ["install", "--local-bin-path", t]
+
+checkShaders :: [FilePath] -> IO ()
+checkShaders = mapM_ glslangValidator
+
+glslangValidator f = _callCmd "glslangValidator" [f] fst
 
 main :: IO ()
 main = do
@@ -23,5 +30,6 @@ main = do
     files <- listDirectory "shaders"
     setCurrentDirectory "shaders"
     shaders <- mapM makeAbsolute files
+    checkShaders shaders
     setCurrentDirectory ".."
     mapM_ (\src -> copyFile src ("target/" ++ (takeFileName src))) shaders
