a90fa482c048fccd13a2d135ac292428c9668731
Mateusz Biel <matbiel@gmail.com>
2019-02-25 20:19:29 +0100

    update

--- src/ObjFile.hs
+++ src/ObjFile.hs
@@ -9,22 +9,31 @@ import Data.Text.Lazy as T
 import System.IO
 import Data.List as L
 
+data ObjData = ObjData {
+    v :: [M.Vector3],
+    n :: [M.Vector3],
+    vecIndices :: [TriangleIndex],
+    normIndices :: [TriangleIndex]
+}
+
+emptyData = ObjData [] [] [] []
 
 readObjFile :: FilePath -> IO Model
 readObjFile path = do
     content <- I.readFile path
-    return $ parseLines (fmap T.unpack $ T.lines content) emptyModel
+    let d = parseLines (fmap T.unpack $ T.lines content) emptyData
+    return $ M.Model (v d) (vecIndices d)
 
-parseLines :: [String] -> Model -> Model
-parseLines [] m = Model (L.reverse $ vertices m) (L.reverse $ indices m)
-parseLines (x:xs) m = let tokens = lexer x in
+parseLines :: [String] -> ObjData -> ObjData
+parseLines [] d = ObjData (L.reverse $ v d) (L.reverse $ n d) (L.reverse $ vecIndices d) (L.reverse $ normIndices d)
+parseLines (x:xs) d = let tokens = lexer x in
                         case tokens of
-                            [] -> parseLines xs m
-                            t -> parseLines xs $ updateModel (objParse t) m
+                            [] -> parseLines xs d
+                            t -> parseLines xs $ updateObjData (objParse t) d
 
-updateModel :: Value -> Model -> Model
-updateModel (ObjParser.Vertex3 x y z) (Model v i) = Model (v3:v) i
+updateObjData :: Value -> ObjData -> ObjData
+updateObjData (ObjParser.Vertex3 x y z) (ObjData v n i ni) = ObjData (v3:v) n i ni
     where v3 = M.Vector3 x y z
-updateModel (ObjParser.FaceDef a1 a2 a3) (Model v i) = Model v (idx:i)
+updateObjData (ObjParser.FaceDef a1 a2 a3) (ObjData v n i ni) = ObjData v n (idx:i) ni
     where idx = TriangleIndex (vIdx a1 - 1) (vIdx a2 - 1) (vIdx a3 - 1)
-updateModel _ m = m
+updateObjData _ m = m
